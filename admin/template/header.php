43794fe1af08044848786529af323d578ff20ea8
eternalsugarzy <irwanfrozen@gmail.com>
2025-12-10 00:37:55 +0800

    Menambahkan data master

---
+++ admin/template/header.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+// Cek Keamanan: Jika belum login, tendang ke login.php
+// Perhatikan path location: ../login.php karena file ini akan di-include di folder admin
+if (!isset($_SESSION['status']) || $_SESSION['status'] != "login") {
+    header("location:login.php?pesan=belum_login");
+    exit;
+}
+
+// Include koneksi (Mundur 1 langkah dari folder admin)
+include '../koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Sistem Kunjungan DPRD</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui" />
+    
+    <link rel="icon" href="../assets/images/favicon.svg" type="image/x-icon" />
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
+    <link rel="stylesheet" href="../assets/fonts/tabler-icons.min.css" />
+    <link rel="stylesheet" href="../assets/fonts/feather.css" />
+    <link rel="stylesheet" href="../assets/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="../assets/fonts/material.css" />
+    
+    <link rel="stylesheet" href="../assets/css/style.css" id="main-style-link" />
+    <link rel="stylesheet" href="../assets/css/style-preset.css" />
+</head>
+
+<body>
+    <div class="loader-bg">
+      <div class="loader-track">
+        <div class="loader-fill"></div>
+      </div>
+    </div>
