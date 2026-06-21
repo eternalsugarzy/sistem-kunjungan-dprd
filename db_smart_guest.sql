60e2eb932c0ccd396be4fb2020af1c499f765bc4
sugarzy <irwanfrozen@gmail.com>
2026-06-21 22:19:55 +0800

    DB Baru

--- db_smart_guest.sql
+++ db_smart_guest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 18, 2026 at 05:44 AM
+-- Generation Time: Jun 21, 2026 at 02:19 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.2.31
 
@@ -36,6 +36,13 @@ CREATE TABLE `admin` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nama_pengguna`, `username`, `password`, `level`, `created_at`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin', '2026-06-18 05:56:14');
+
 -- --------------------------------------------------------
 
 --
@@ -54,6 +61,13 @@ CREATE TABLE `buku_tamu` (
   `status_ttd` enum('valid','invalid','kosong') DEFAULT 'kosong',
   `waktu_hadir` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `buku_tamu`
+--
+
+INSERT INTO `buku_tamu` (`id_tamu`, `id_kunjungan`, `nama_peserta`, `asal_instansi`, `jabatan_peserta`, `no_hp`, `tanda_tangan`, `timestamp_ttd`, `status_ttd`, `waktu_hadir`) VALUES
+(1, 1, 'Junady', 'DPRD Kab.Tala', 'Staf', '08134568789', 'TTD_CANVAS_1781764293_927.png', NULL, 'kosong', '2026-06-18 06:31:33');
 
 -- --------------------------------------------------------
 
@@ -125,6 +139,13 @@ CREATE TABLE `kategori_kunjungan` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `kategori_kunjungan`
+--
+
+INSERT INTO `kategori_kunjungan` (`id_kategori`, `nama_kategori`, `kode_kategori`, `deskripsi`, `is_active`, `created_at`) VALUES
+(2, 'Studi Tiru', 'STD-01', 'Kunjungan komparatif adopsi rancangan peraturan daerah.	\r\n', 1, '2026-06-18 06:20:58');
+
 -- --------------------------------------------------------
 
 --
@@ -155,7 +176,7 @@ CREATE TABLE `kunjungan` (
   `id_pj` int DEFAULT NULL,
   `id_ruangan` int DEFAULT NULL,
   `id_admin_verif` int DEFAULT NULL,
-  `status_kegiatan` enum('pending','dijadwalkan','selesai','batal') DEFAULT 'pending',
+  `status_kegiatan` enum('pending','dijadwalkan','sedang berkunjung','selesai','batal') DEFAULT 'pending',
   `alasan_pembatalan` text,
   `tgl_pembatalan` datetime DEFAULT NULL,
   `qr_code_data` varchar(500) DEFAULT NULL,
@@ -166,6 +187,17 @@ CREATE TABLE `kunjungan` (
   `waktu_checkout` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kunjungan`
+--
+
+INSERT INTO `kunjungan` (`id_kunjungan`, `kode_booking`, `email_pemohon`, `no_hp_pemohon`, `no_register`, `jenis_pendaftaran`, `id_kategori`, `tgl_surat_permohonan`, `tgl_kunjungan`, `waktu_kunjungan`, `nama_instansi_tamu`, `alamat_instansi`, `jumlah_peserta_rencana`, `materi_kunjungan`, `file_surat_permohonan`, `no_skk`, `tgl_skk`, `instansi_pengirim_skk`, `perihal_skk`, `file_skk`, `id_pj`, `id_ruangan`, `id_admin_verif`, `status_kegiatan`, `alasan_pembatalan`, `tgl_pembatalan`, `qr_code_data`, `qr_code_path`, `status_kehadiran`, `waktu_scan`, `batas_waktu_kunjungan`, `waktu_checkout`, `created_at`) VALUES
+(1, 'REQ-2026-C2C3', 'tala.dprd@mail.com', '08123456783', NULL, 'online', NULL, '2026-06-07', '2026-06-18', '15:00:00', 'DPRD Kab.Tala', 'Jl A Yani', 20, 'Rapat Anggaran 2026', 'SURAT_1781763648.png', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL, 'selesai', NULL, NULL, NULL, 'uploads/qr/QR_REQ-2026-C2C3.png', 'hadir', '2026-06-18 06:22:04', NULL, NULL, '2026-06-18 06:20:48'),
+(2, 'REQ-2026-798C', 'trinity@mail.com', '487943179', NULL, 'online', 2, '2026-06-01', '2026-06-18', '19:00:00', 'Trinity Scent', 'Jl Pembangunan Hasan Basri', 10, 'Rapat Anggaran 2027', 'SURAT_1781765840.png', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL, 'selesai', NULL, NULL, NULL, 'uploads/qr/QR_REQ-2026-798C.png', 'hadir', '2026-06-18 06:59:15', NULL, NULL, '2026-06-18 06:57:20'),
+(3, 'REQ-2026-2E29', 'admin@mail.com.id', '08123456783', NULL, 'online', 2, '2026-06-08', '2026-06-18', '15:10:00', 'DPRD Provinsi Kalimantan Selatan', 'Jl Lambung Mangkurat', 5, 'Konsultasi Terkait Proyek Jembatan Flyover Kota Banjarbaru', 'SURAT_1781766493.png', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL, 'selesai', NULL, NULL, NULL, 'uploads/qr/QR_REQ-2026-2E29.png', 'hadir', '2026-06-18 08:00:47', NULL, NULL, '2026-06-18 07:08:13'),
+(4, 'REQ-2026-4924', 'uniska.bem@sch.id', '089854123301', NULL, 'online', NULL, '2026-06-08', '2026-06-20', '10:20:00', 'BEM Universitas Islam Kalimantan MAB', 'Jl Adhyaksa', 15, 'Konsultasi MBG', 'SURAT_1781767039.png', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL, 'selesai', NULL, NULL, NULL, 'uploads/qr/QR_REQ-2026-4924.png', 'selesai', '2026-06-18 07:18:26', NULL, '2026-06-18 07:58:57', '2026-06-18 07:17:19'),
+(5, 'REQ-2026-1AE6', 'dispar.bjm@gmail.com', '085474556301', NULL, 'online', 2, '2026-06-10', '2026-06-22', '08:30:00', 'Dinas Pariwisata Kota Banjarmasin', 'Jl Pangeran Hidayatullah Banjarmasin', 7, 'Konsultasi Terkait Destinasi Wisata Kota Banjarmasin', 'SURAT_1781769922.png', NULL, NULL, NULL, NULL, NULL, 1, 1, NULL, 'sedang berkunjung', NULL, NULL, NULL, 'uploads/qr/QR_REQ-2026-1AE6.png', 'hadir', '2026-06-18 08:07:39', NULL, NULL, '2026-06-18 08:05:22');
 
 -- --------------------------------------------------------
 
@@ -182,6 +214,14 @@ CREATE TABLE `penanggung_jawab` (
   `no_hp` varchar(20) DEFAULT NULL,
   `file_ttd` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `penanggung_jawab`
+--
+
+INSERT INTO `penanggung_jawab` (`id_pj`, `nama_pj`, `nip`, `pangkat_golongan`, `jabatan`, `no_hp`, `file_ttd`) VALUES
+(1, 'Ahmad Saiduns, S.Kom', '212021212102', NULL, 'Kasubbag Humas', '08123456783', ''),
+(2, 'Budi Irawan, S.H', '21202121210', 'Pembina Utama Madya', 'Sekretaris', '08123456783', 'PJ_TTD_C_1781770825_169.png');
 
 -- --------------------------------------------------------
 
@@ -213,6 +253,13 @@ CREATE TABLE `ruangan` (
   `kapasitas` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dumping data for table `ruangan`
+--
+
+INSERT INTO `ruangan` (`id_ruangan`, `nama_ruangan`, `lantai`, `kapasitas`) VALUES
+(1, 'Ruang Komis 1', 'Lantai 1', 40);
+
 -- --------------------------------------------------------
 
 --
@@ -231,6 +278,14 @@ CREATE TABLE `spt_tugas` (
   `jumlah_ditugaskan` int DEFAULT NULL,
   `file_spt` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `spt_tugas`
+--
+
+INSERT INTO `spt_tugas` (`id_spt`, `id_kunjungan`, `jenis_petugas`, `no_spt`, `tgl_spt`, `nama_pegawai`, `nip`, `jabatan`, `jumlah_ditugaskan`, `file_spt`) VALUES
+(1, 4, 'pendamping', '090/DPRD/BJM/2026', '2026-06-18', 'Ahmad Said, S.Kom', '', 'Kasubbag Humas', 10, NULL),
+(2, 5, 'pendamping', '090/DPRD/BJM/2026', '2026-06-18', 'Ahmad Said, S.Kom', '', 'Kasubbag Humas', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -349,13 +404,13 @@ ALTER TABLE `statistik_kunjungan`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_admin` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `buku_tamu`
 --
 ALTER TABLE `buku_tamu`
-  MODIFY `id_tamu` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tamu` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `disposisi_kunjungan`
@@ -379,19 +434,19 @@ ALTER TABLE `jadwal_pejabat`
 -- AUTO_INCREMENT for table `kategori_kunjungan`
 --
 ALTER TABLE `kategori_kunjungan`
-  MODIFY `id_kategori` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kategori` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `kunjungan`
 --
 ALTER TABLE `kunjungan`
-  MODIFY `id_kunjungan` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kunjungan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `penanggung_jawab`
 --
 ALTER TABLE `penanggung_jawab`
-  MODIFY `id_pj` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pj` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `qr_scan_log`
@@ -403,13 +458,13 @@ ALTER TABLE `qr_scan_log`
 -- AUTO_INCREMENT for table `ruangan`
 --
 ALTER TABLE `ruangan`
-  MODIFY `id_ruangan` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ruangan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `spt_tugas`
 --
 ALTER TABLE `spt_tugas`
-  MODIFY `id_spt` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id_spt` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `statistik_kunjungan`
